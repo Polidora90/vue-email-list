b99e9c3e7b8406663a366f1c271071788fc40ff1
Polidora90 <alessandra.torry@gmail.com>
2021-05-11 18:37:48 +0200

    la lista compare solo quando è stata generata

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ new Vue({
         listLength: 10
     },
     mounted() {
+
+        const ajaxMail = [];
+
         for (let i = 0; i < this.listLength; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
-                this.randomMail.push(resp.data.response);
+                ajaxMail.push(resp.data.response);
+
+                if (ajaxMail.length === this.listLength) {
+                    this.randomMail.push(...ajaxMail);
+                }
             })
         }
     }
